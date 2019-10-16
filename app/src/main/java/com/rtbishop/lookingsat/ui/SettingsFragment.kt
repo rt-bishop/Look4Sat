42b79144822b3db37008cedd94be71f40d0f1210
Arty Bishop <bishop.arty@gmail.com>
2019-10-16 20:34:56 +0100

    Add preference library and settings fragment placeholder

--- app/src/main/java/com/rtbishop/lookingsat/ui/SettingsFragment.kt
+++ app/src/main/java/com/rtbishop/lookingsat/ui/SettingsFragment.kt
@@ -1,24 +1,11 @@
 package com.rtbishop.lookingsat.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import com.rtbishop.lookingsat.MainViewModel
+import androidx.preference.PreferenceFragmentCompat
 import com.rtbishop.lookingsat.R
 
-class SettingsFragment : Fragment() {
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_sky, container, false)
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference, rootKey)
     }
 }
