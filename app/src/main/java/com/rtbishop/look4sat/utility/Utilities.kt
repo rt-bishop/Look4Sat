24ab1b0990e2fff136712c7cba25e4bdee02386e
Arty Bishop <bishop.arty@gmail.com>
2020-11-29 17:36:43 +0000

    v2.0.0 - New BottomNavigation UI, OSM MapView, major code refactoring

--- app/src/main/java/com/rtbishop/look4sat/utility/Utilities.kt
+++ app/src/main/java/com/rtbishop/look4sat/utility/Utilities.kt
@@ -1,9 +1,25 @@
+/*******************************************************************************
+ Look4Sat. Amateur radio satellite tracker and pass predictor.
+ Copyright (C) 2019, 2020 Arty Bishop (bishop.arty@gmail.com)
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************/
+
 package com.rtbishop.look4sat.utility
 
-import android.content.SharedPreferences
-import android.view.View
 import com.github.amsacode.predict4java.GroundStationPosition
-import com.google.android.material.snackbar.Snackbar
 import java.util.concurrent.TimeUnit
 import kotlin.math.round
 
@@ -21,14 +37,6 @@ object Utilities {
         var multiplier = 1.0
         repeat(decimals) { multiplier *= 10 }
         return round(this * multiplier) / multiplier
-    }
-
-    fun SharedPreferences.Editor.putDouble(key: String, double: Double) {
-        putLong(key, double.toRawBits())
-    }
-
-    fun SharedPreferences.getDouble(key: String, default: Double): Double {
-        return Double.fromBits(getLong(key, default.toRawBits()))
     }
 
     fun qthToGSP(qthString: String): GroundStationPosition {
