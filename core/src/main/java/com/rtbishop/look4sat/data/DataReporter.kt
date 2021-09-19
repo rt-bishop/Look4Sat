84240c94c6995931ae7659301869fcfc65136be2
Arty Bishop <bishop.arty@gmail.com>
2021-09-19 17:09:47 +0100

    Added frequency reporting to PassReporter.kt

--- core/src/main/java/com/rtbishop/look4sat/data/DataReporter.kt
+++
@@ -1,8 +0,0 @@
-package com.rtbishop.look4sat.data
-
-interface DataReporter {
-
-    suspend fun setRotatorSocket(server: String, port: Int)
-
-    suspend fun reportRotation(azim: Double, elev: Double)
-}
