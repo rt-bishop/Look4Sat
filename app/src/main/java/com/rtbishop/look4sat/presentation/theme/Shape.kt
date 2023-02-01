411ac4179dfa566736b4bb1023bbdda99f83d156
Arty Bishop <bishop.arty@gmail.com>
2023-02-01 16:24:44 +0000

    Recreated About screen in Compose

--- app/src/main/java/com/rtbishop/look4sat/presentation/theme/Shape.kt
+++ app/src/main/java/com/rtbishop/look4sat/presentation/theme/Shape.kt
@@ -5,7 +5,7 @@ import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(8.dp),
-    medium = RoundedCornerShape(8.dp),
-    large = RoundedCornerShape(8.dp)
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(12.dp),
+    large = RoundedCornerShape(12.dp)
 )
