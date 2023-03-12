1f63cea626d452fbaf038331bd90aaf9595b2696
Arty Bishop <bishop.arty@gmail.com>
2023-03-12 13:04:12 +0000

    More fixes for SettingsScreen.kt and its states

---
+++ app/src/main/java/com/rtbishop/look4sat/presentation/settingsScreen/SettingsScreenState.kt
@@ -0,0 +1,33 @@
+package com.rtbishop.look4sat.presentation.settingsScreen
+
+data class LocationSettings(
+    val getUpdating: Boolean,
+    val getLastUpdated: Long,
+    val getLatitude: Double,
+    val getLongitude: Double,
+    val getLocator: String,
+    val setGpsLoc: () -> Unit,
+    val setManualLoc: (Double, Double) -> Unit,
+    val setQthLoc: (String) -> Unit
+)
+
+data class DataSettings(
+    val getUpdating: Boolean,
+    val getLastUpdated: Long,
+    val getSatellites: Int,
+    val getRadios: Int,
+    val updateFromWeb: () -> Unit,
+    val updateFromFile: (String) -> Unit,
+    val clearAllData: () -> Unit
+)
+
+data class OtherSettings(
+    val getUtc: Boolean,
+    val getUpdate: Boolean,
+    val getSweep: Boolean,
+    val getSensor: Boolean,
+    val setUtc: (Boolean) -> Unit,
+    val setUpdate: (Boolean) -> Unit,
+    val setSweep: (Boolean) -> Unit,
+    val setSensor: (Boolean) -> Unit
+)
