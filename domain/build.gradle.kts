272a03e2d98453e2ea1ce0f81952ef24a4e52261
Arty Bishop <bishop.arty@gmail.com>
2023-07-29 12:33:14 +0100

    Migrated project build files to use kotlin script

---
+++ domain/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("kotlin")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+}
