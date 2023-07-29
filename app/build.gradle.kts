71ee49dbfe9777c1717144a935f65e961acc23b7
Arty Bishop <bishop.arty@gmail.com>
2023-07-29 12:33:14 +0100

    Migrated project build files to use kotlin script

---
+++ app/build.gradle.kts
@@ -0,0 +1,102 @@
+@file: Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.application")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.rtbishop.look4sat"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.rtbishop.look4sat"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 313
+        versionName = "3.1.3"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        compose = true
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    testOptions {
+        unitTests {
+            all { it.useJUnitPlatform() }
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.7"
+    }
+    packaging {
+        resources {
+            excludes += listOf(
+                "/META-INF/{AL2.0,LGPL2.1}",
+                "META-INF/DEPENDENCIES.txt",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE.txt",
+                "META-INF/NOTICE",
+                "META-INF/LICENSE",
+                "META-INF/DEPENDENCIES",
+                "META-INF/notice.txt",
+                "META-INF/license.txt",
+                "META-INF/dependencies.txt",
+                "META-INF/LGPL2.1",
+                "META-INF/maven/commons-io/commons-io/pom.xml",
+                "META-INF/maven/commons-io/commons-io/pom.properties"
+            )
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(project(":data"))
+    implementation(project(":domain"))
+    // Android
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+    // Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.animation:animation:1.4.3")
+    implementation("androidx.compose.compiler:compiler:1.5.1")
+    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.runtime:runtime:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    // Utility
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.16")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.3")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
+    // Test
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
+}
