789d3923642767b3e11e2f554aa96409ccb47fab
Arty Bishop <bishop.arty@gmail.com>
2023-08-12 17:33:27 +0100

    Updated compileSdk and targetSdk to Android 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ kotlin {
 
 android {
     namespace = "com.rtbishop.look4sat"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.rtbishop.look4sat"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 313
         versionName = "3.1.3"
         resourceConfigurations.addAll(listOf("en", "ru", "si", "zh-rCN", "anydpi"))
@@ -44,22 +44,22 @@ dependencies {
     ksp("androidx.room:room-compiler:2.5.2")
 
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.animation:animation:1.4.3")
+    implementation("androidx.compose.animation:animation:1.5.0")
     implementation("androidx.compose.compiler:compiler:1.5.1")
     implementation("androidx.compose.material3:material3:1.1.1")
-    implementation("androidx.compose.runtime:runtime:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation("androidx.compose.runtime:runtime:1.5.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("org.osmdroid:osmdroid-android:6.1.16")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk:1.13.5")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "7.1.2" apply false
+    id("com.android.library") version "8.1.0" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
 }
 
 tasks.register("clean", Delete::class.java) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
