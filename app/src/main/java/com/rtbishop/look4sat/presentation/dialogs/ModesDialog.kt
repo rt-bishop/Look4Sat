744d162a7d096f582fe4be2478d8050d75583d74
Arty Bishop <bishop.arty@gmail.com>
2023-08-13 16:06:57 +0100

    Added minor tweaks to Dialogs and layouts

--- app/src/main/java/com/rtbishop/look4sat/presentation/dialogs/ModesDialog.kt
+++ app/src/main/java/com/rtbishop/look4sat/presentation/dialogs/ModesDialog.kt
@@ -5,11 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
@@ -25,54 +24,49 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.rtbishop.look4sat.presentation.MainTheme
 
-private val allModes = listOf(
-    "AFSK", "AFSK S-Net", "AFSK SALSAT", "AHRPT", "AM", "APT", "BPSK", "BPSK PMT-A3",
-    "CERTO", "CW", "DQPSK", "DSTAR", "DUV", "FFSK", "FM", "FMN", "FSK", "FSK AX.100 Mode 5",
-    "FSK AX.100 Mode 6", "FSK AX.25 G3RUH", "GFSK", "GFSK Rktr", "GMSK", "HRPT", "LoRa",
-    "LRPT", "LSB", "MFSK", "MSK", "MSK AX.100 Mode 5", "MSK AX.100 Mode 6", "OFDM", "OQPSK",
-    "PSK", "PSK31", "PSK63", "QPSK", "QPSK31", "QPSK63", "SSTV", "USB", "WSJT"
-)
-
 @Preview(showBackground = true)
 @Composable
 private fun ModesDialogPreview() {
+    val allModes = listOf(
+        "AFSK", "AFSK S-Net", "AFSK SALSAT", "AHRPT", "AM", "APT", "BPSK", "BPSK PMT-A3",
+        "CERTO", "CW", "DQPSK", "DSTAR", "DUV", "FFSK", "FM", "FMN", "FSK", "FSK AX.100 Mode 5",
+        "FSK AX.100 Mode 6", "FSK AX.25 G3RUH", "GFSK", "GFSK Rktr", "GMSK", "HRPT", "LoRa",
+        "LRPT", "LSB", "MFSK", "MSK", "MSK AX.100 Mode 5", "MSK AX.100 Mode 6", "OFDM", "OQPSK",
+        "PSK", "PSK31", "PSK63", "QPSK", "QPSK31", "QPSK63", "SSTV", "USB", "WSJT"
+    )
     MainTheme { ModesDialog(allModes, listOf("AFSK", "AFSK S-Net"), {}) {} }
 }
 
 @Composable
-fun ModesDialog(list: List<String>, selected: List<String>, toggle: () -> Unit, click: (String) -> Unit) {
-    Dialog(onDismissRequest = { toggle() }) {
-        ElevatedCard(modifier = Modifier.fillMaxHeight(0.9f)) {
+fun ModesDialog(items: List<String>, selected: List<String>, dismiss: () -> Unit, select: (String) -> Unit) {
+    Dialog(onDismissRequest = { dismiss() }) {
+        ElevatedCard(modifier = Modifier.fillMaxHeight(0.75f)) {
             LazyVerticalGrid(
-                columns = GridCells.Fixed(1),
+                columns = GridCells.Adaptive(160.dp),
                 modifier = Modifier.background(MaterialTheme.colorScheme.background),
                 horizontalArrangement = Arrangement.spacedBy(1.dp),
                 verticalArrangement = Arrangement.spacedBy(1.dp)
             ) {
-                items(list) { mode ->
+                itemsIndexed(items) { index, item ->
                     Surface {
                         Row(verticalAlignment = Alignment.CenterVertically,
                             modifier = Modifier
                                 .background(MaterialTheme.colorScheme.surface)
-                                .fillMaxWidth()
-                                .clickable {
-                                    click(mode)
-                                    toggle()
-                                }) {
+                                .clickable { select(item) }) {
                             Text(
-                                text = mode,
-                                modifier = Modifier
-                                    .padding(start = 8.dp, end = 6.dp)
-                                    .weight(1f),
+                                text = "$index).",
+                                modifier = Modifier.padding(start = 12.dp, end = 6.dp),
                                 fontWeight = FontWeight.Normal,
+                                color = MaterialTheme.colorScheme.secondary
+                            )
+                            Text(
+                                text = item,
+                                modifier = Modifier.weight(1f),
+                                fontWeight = FontWeight.Medium,
                                 maxLines = 1,
                                 overflow = TextOverflow.Ellipsis
                             )
-                            Checkbox(
-                                checked = selected.contains(mode),
-                                onCheckedChange = null,
-                                modifier = Modifier.padding(6.dp)
-                            )
+                            Checkbox(checked = selected.contains(item), onCheckedChange = { select(item) })
                         }
                     }
                 }
