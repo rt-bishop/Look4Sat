a6b5498f556d5839907ce7ebb350b8d737aa0cff
Arty Bishop <bishop.arty@gmail.com>
2023-10-01 15:47:12 +0100

    Added type reset for satellite selection screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.runtime:runtime:1.5.2")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.3")
 
