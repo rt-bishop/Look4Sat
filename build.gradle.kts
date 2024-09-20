0849d53c58757dfdd90cdfc50c2407b6348e002b
Arty Bishop <bishop.arty@gmail.com>
2024-09-20 11:23:42 +0100

    Updated dependencies and gradle plugin, minor fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
