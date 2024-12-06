86512a44d2a8dd6092574451e15a9608310ec242
Arty Bishop <bishop.arty@gmail.com>
2024-12-06 14:45:56 +0000

    Updated dependencies and gradle plugin, minor fixes

--- app/src/main/java/com/rtbishop/look4sat/presentation/components/Common.kt
+++ app/src/main/java/com/rtbishop/look4sat/presentation/components/Common.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
