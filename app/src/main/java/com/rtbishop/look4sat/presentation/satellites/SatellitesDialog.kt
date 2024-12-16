cdf642d9f3b3dab1ad9d48f49ba1f19682feae9a
Arty Bishop <bishop.arty@gmail.com>
2024-12-16 10:27:50 +0000

    Added multi selection for Satellite types filter dialog

--- app/src/main/java/com/rtbishop/look4sat/presentation/satellites/SatellitesDialog.kt
+++ app/src/main/java/com/rtbishop/look4sat/presentation/satellites/SatellitesDialog.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -25,16 +27,21 @@ import com.rtbishop.look4sat.presentation.components.SharedDialog
 
 @Preview(showBackground = true)
 @Composable
-private fun TypeDialogPreview() {
+private fun MultiTypesDialogPreview() {
     val types = listOf("All", "Amateur", "Geostationary", "Military", "Weather")
-    MainTheme { TypesDialog(types, "All", {}) {} }
+    MainTheme { MultiTypesDialog(types, emptyList(), {}) {} }
 }
 
 @Composable
-fun TypesDialog(
-    items: List<String>, selected: String, dismiss: () -> Unit, select: (String) -> Unit
+fun MultiTypesDialog(
+    allTypes: List<String>, types: List<String>, cancel: () -> Unit, accept: (List<String>) -> Unit
 ) {
-    SharedDialog(title = "Select category", onCancel = dismiss, onAccept = {}) {
+    val selected = remember { mutableStateListOf<String>().apply { addAll(types) } }
+    val select = { type: String ->
+        if (selected.contains(type)) selected.remove(type) else selected.add(type)
+    }
+    val onAccept = { accept(selected.toList()) }
+    SharedDialog(title = "Select category", onCancel = cancel, onAccept = onAccept) {
         LazyVerticalGrid(
             columns = GridCells.Adaptive(240.dp),
             modifier = Modifier
@@ -44,11 +51,13 @@ fun TypesDialog(
             verticalArrangement = Arrangement.spacedBy(1.dp)
         ) {
             item { HorizontalDivider(color = MaterialTheme.colorScheme.surface) }
-            itemsIndexed(items) { index, item ->
-                Row(verticalAlignment = Alignment.CenterVertically,
+            itemsIndexed(allTypes) { index, item ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .background(MaterialTheme.colorScheme.surface)
-                        .clickable { select(item) }) {
+                        .clickable { select(item) }
+                ) {
                     Text(
                         text = "${index + 1}).",
                         modifier = Modifier.padding(start = 16.dp, end = 8.dp),
@@ -62,9 +71,9 @@ fun TypesDialog(
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis
                     )
-                    RadioButton(
-                        selected = item == selected,
-                        onClick = null,
+                    Checkbox(
+                        checked = selected.contains(item),
+                        onCheckedChange = null,
                         modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
                     )
                 }
