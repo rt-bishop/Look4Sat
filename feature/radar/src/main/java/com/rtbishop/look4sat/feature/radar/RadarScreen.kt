9699d142fa804af904d402e72c084ce76fb140d7
Arty Bishop <bishop.arty@gmail.com>
2026-09-13 15:56:30 +0100

    Fixed RadarScreen padding, implemented basic Detekt setup

--- feature/radar/src/main/java/com/rtbishop/look4sat/feature/radar/RadarScreen.kt
+++ feature/radar/src/main/java/com/rtbishop/look4sat/feature/radar/RadarScreen.kt
@@ -31,9 +31,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -115,6 +118,7 @@ private fun RadarScreen(
     Column(
         modifier = Modifier
             .layoutPadding()
+            .padding(bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding())
             .keepScreenOn(),
         verticalArrangement = Arrangement.spacedBy(6.dp)
     ) {
